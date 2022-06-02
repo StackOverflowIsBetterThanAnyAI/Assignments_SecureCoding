fd3ef7e142ce1ad8ceed2de57c52f599fa650aa7
lacrit00 <lacrit00@hs-esslingen.de>
2022-06-02 16:17:59 +0200

    change cwe131

--- Code_CWE131/cwe131.c
+++ Code_CWE131/cwe131.c
@@ -7,11 +7,15 @@ int main(int argc, char const *argv[])
     int *id_sequenz;
     
     /* Speicherplatz für ein Array mit 3 IDs reservieren*/
-    id_sequenz = (int*) malloc(3);
-    if (id_sequenz == NULL) exit(1);
+    /*id_sequenz = (int*) malloc(3);
+    if (id_sequenz == NULL) exit(1);*/
 
-    /* Das Array mit den IDs befüllen */
-    id_sequenz[0] = 13579;
+    /* Das Array mit den IDs befüllen lassen*/
+    for(int i=0; i<3; i++)
+    {
+        printf("Geben Sie die fünfstellige ID ein: ");
+    }
+   /* id_sequenz[0] = 13579;
     id_sequenz[1] = 24680;
-    id_sequenz[2] = 97531;
+    id_sequenz[2] = 97531;*/
 }
