550f77b1183e6bc85ad7180f57f2d98bd098bd9b
lacrit00 <lacrit00@hs-esslingen.de>
2022-06-04 22:31:27 +0200

    Adding CWE122

---
+++ Code_CWE122/CWE122.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 256
+
+int main(int argc, char **argv) //Argumente für Kommandozeile
+{
+    char *buf;
+    buf = (char*)malloc(sizeof(char)*BUFSIZE);
+    strcpy(buf, argv[1]);
+    printf(argv[1]);
+}
+
